c8db8c46a31631005df718b68e7bb9db363d20bf
myles91 <myles.sanigar@gmail.com>
2017-06-20 22:32:14 +0100

    MS : fix the dist/build path error...
    Should now just "work" on npm run start

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     './styles/main.scss'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/build/'
   },
