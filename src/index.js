b3e97054f929ca1474924c0724cb7c81836c24f6
myles91 <myles.sanigar@gmail.com>
2017-07-05 23:34:23 +0100

    MS : better autoprefixing, ie9 detection...
    Now to build an ie9 compatible spinner

--- src/index.js
+++ src/index.js
@@ -73,3 +73,11 @@ window.testInstance = new Spinner({
   entry: '.test',
   spinnerColor: 'FireBrick'
 });
+
+if(navigator.appVersion.indexOf("MSIE 9.")!=-1) {
+  fallBackSpinner();
+}
+
+function fallBackSpinner() {
+  console.log("looks like you're using IE9!  we'll serve you a compatible loading spinner...");
+}
